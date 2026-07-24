0f7f15ced87ca6413363264cd07b59497ffa84e3
ThangNS <nsthang1991@gmail.com>
2026-07-24 21:55:08 +0700

    Cập nhật

--- express-session/main.js
+++ express-session/main.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const session = require("express-session");
 const app = express();
-
+const path = require("path");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/public", express.static(path.join(__dirname, "public/css")));
+app.use("/public", express.static(path.join(__dirname, "public/js")));
+app.use("/public", express.static(path.join(__dirname, "public/images")));
 
 app.use(
   session({
@@ -11,33 +15,62 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      maxAge: 60000,
+      maxAge: 1000 * 60 * 60,
     },
   }),
 );
 
 app.get("/", (req, res) => {
-  if (req.session.views) {
-    req.session.views++;
-    res.set("Content-Type", "text/html");
-    res.write(
-      "<p> Session expires after 1 min of in activity: " +
-        req.session.cookie.expires +
-        "</p>",
-    );
-    res.end();
+  if (req.session.user) {
+    res.sendFile(path.join(__dirname, "views/index.html"));
   } else {
-    req.session.views = 1;
-    res.end(" New session is started");
+    req.session.user = {
+      name: "Thang",
+      age: 35,
+    };
+    res.send("Da tao session moi! Hay truy cap /profile ngay bay gio.");
   }
 });
 
-app.get("/profile", (req, res) => {
-  if (req.session.views) {
-    res.end("Profile page");
+app.get("/contact", (req, res) => {
+  if (req.session.user) {
+    res.status(200);
+    res.sendFile(path.join(__dirname, "views/contact.html"));
   } else {
     res.status(404);
-    res.end("Not found");
+    res.sendFile(path.join(__dirname, "views/error.html"));
+  }
+});
+
+app.get("/courses", (req, res) => {
+  if (req.session.user) {
+    res.status(200);
+    res.sendFile(path.join(__dirname, "views/courses.html"));
+  } else {
+    res.status(404);
+    res.sendFile(path.join(__dirname, "views/error.html"));
+  }
+});
+
+app.get("/thanks", (req, res) => {
+  if (req.session.user) {
+    res.status(200);
+    res.sendFile(path.join(__dirname, "views/thanks.html"));
+  } else {
+    res.status(404);
+    res.sendFile(path.join(__dirname, "views/error.html"));
+  }
+});
+
+app.post("/", (req, res) => {
+  if (req.session.user) {
+    const { email } = req.body;
+    console.log(email);
+    res.status(200);
+    res.sendFile(path.join(__dirname, "views/thanks.html"));
+  } else {
+    res.status(404);
+    res.sendFile(path.join(__dirname, "views/error.html"));
   }
 });
 
